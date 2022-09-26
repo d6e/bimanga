0f106fc27451543d690cbfcf6aca5f8de4bcb6d8
d6e <git@d6e.io>
2022-09-25 17:48:55 -0700

    Add traversibility.

--- main.py
+++ main.py
@@ -14,24 +14,18 @@ def index():
     return flask.render_template('index.html', items=os.listdir(library))
 
 
-# @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def get_dir(path):
-    path = escape(path)
-    return flask.render_template('index.html', path=path, items=os.listdir(pathlib.Path(library, path)))
-
-
-# @app.route("/<title>")
-# def get_page1(title):
-#     title = escape(title)
-#     return flask.render_template('index.html', name='Languages', items=os.listdir(pathlib.Path(library, title)))
+@app.route("/<title>")
+def get_page1(title):
+    title = escape(title)
+    return flask.render_template('lang.html', path=title, items=os.listdir(pathlib.Path(library, title)))
 
 
 @app.route("/<title>/<lang>")
 def get_page2(title, lang):
     title = escape(title)
     lang = escape(lang)
-    return flask.render_template('index.html', name='Chapters', path="title/",
+    path = title + "/" + lang
+    return flask.render_template('chapters.html', name='Chapters', path=path,
                                  items=os.listdir(pathlib.Path(library, title, lang)))
 
 
@@ -40,7 +34,9 @@ def get_page3(title, lang, chapter):
     title = escape(title)
     lang = escape(lang)
     chapter = escape(chapter)
-    return os.listdir(pathlib.Path(library, title, lang, chapter))
+    path = title + "/" + lang + "/" + chapter
+    return flask.render_template('pages.html', name='Chapters', path=path,
+                                 items=os.listdir(pathlib.Path(library, title, lang, chapter)))
 
 
 @app.route("/<title>/<lang>/<chapter>/<page>")
