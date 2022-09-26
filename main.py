7fd232a2999097e0a42d595f2be8cdc4c9ed2707
d6e <git@d6e.io>
2022-09-25 17:30:24 -0700

    init

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import pathlib
+
+import flask
+from flask import Flask, send_file
+from markupsafe import escape
+
+app = Flask(__name__)
+library = 'manga'
+
+
+
+@app.route("/")
+def index():
+    return flask.render_template('index.html', name='Manga', items=os.listdir(library))
+
+
+@app.route("/<title>")
+def get_page1(title):
+    title = escape(title)
+    return flask.render_template('index.html', name='Languages', items=os.listdir(pathlib.Path(library, title)))
+
+
+@app.route("/<title>/<lang>")
+def get_page2(title, lang):
+    title = escape(title)
+    lang = escape(lang)
+    return flask.render_template('index.html', name='Chapters', path="title/", items=os.listdir(pathlib.Path(library, title, lang)))
+
+
+@app.route("/<title>/<lang>/<chapter>")
+def get_page3(title, lang, chapter):
+    title = escape(title)
+    lang = escape(lang)
+    chapter = escape(chapter)
+    return os.listdir(pathlib.Path(library, title, lang, chapter))
+
+
+@app.route("/<title>/<lang>/<chapter>/<page>")
+def get_page4(title, lang, chapter, page):
+    title = escape(title)
+    lang = escape(lang)
+    chapter = escape(chapter)
+    page = escape(page)
+    path = pathlib.Path(library, title, lang, chapter, page)
+    if not path.exists():
+        return 404
+    return send_file(path, mimetype='image/gif')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
