02359ad3e655fd9bc7ef3c2e80b3756ab1867c25
d6e <git@d6e.io>
2022-10-15 19:11:24 -0700

    update

--- main.py
+++ main.py
@@ -1,53 +1,92 @@
 import os
 import pathlib
 
-import flask
-from flask import Flask, send_file
-from markupsafe import escape
+html = r"""
 
-library = 'manga'
-app = Flask(__name__,  static_folder=library)
+<!doctype html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bilingual Manga</title>
+        <style>
+        html {{
+          max-width: 70ch;
+          padding: 3em 1em;
+          margin: auto;
+          line-height: 1.75;
+          font-size: 1.25em;
+        }}
+        </style>
+    </head>
+    <body>
+        <div id="content">
+            <button onclick="location.href='{prev}'" type="button">prev</button>
+            <button onclick="location.href=''" type="button">jpn</button>
+            <button onclick="location.href='{next}'" type="button">next</button>
+            <img src="{current}">
+            <button onclick="location.href='{prev}'" type="button">prev</button>
+            <button onclick="location.href=''" type="button">jpn</button>
+            <button onclick="location.href='{next}'" type="button">next</button>
+            <br>
+            <form id="form" role="search">
+              <input type="search" id="query" name="q"
+               placeholder="jisho.org"
+               aria-label="Search via jisho.org">
+              <button>Search</button>
+            </form>
+        </div>
+    </body>
+    <footer>
+        <script>
+            const f = document.getElementById('form');
+            const q = document.getElementById('query');
+            const jisho = 'https://jisho.org/search/';
+
+            function submitted(event) {{
+              event.preventDefault();
+              const url = jisho + q.value;
+              const win = window.open(url, '_blank');
+              win.focus();
+            }}
+            f.addEventListener('submit', submitted);
+        </script>
+    </footer>
+</html>
+"""
 
 
-@app.route("/")
-def index():
-    return flask.render_template('index.html', items=os.listdir(library))
+def replace_extension(path, new_extension):
+    return path.with_name(path.name.split('.')[0] + new_extension)
 
 
-@app.route("/<title>")
-def get_page1(title):
-    title = escape(title)
-    return flask.render_template('lang.html', path=title, items=os.listdir(pathlib.Path(library, title)))
+def to_html_url(path):
+    if path == '':
+        return path
+    path = pathlib.Path(path)
+    path = replace_extension(path, ".html")
+    return 'file:///' + path.absolute().as_posix()
 
 
-@app.route("/<title>/<lang>")
-def get_page2(title, lang):
-    title = escape(title)
-    lang = escape(lang)
-    path = title + "/" + lang
-    return flask.render_template('chapters.html', name='Chapters', path=path,
-                                 items=os.listdir(pathlib.Path(library, title, lang)))
+def main():
+    """ The library file tree is assumed to be <title>/<3-digit language code>/<chapter>/<page>"""
+    site = pathlib.Path("site")
+    assets = pathlib.Path("manga")
+    image_paths = assets.glob("**/*.jpg")
+    prev_path = ''
+    current_path = ''
+    for next_path in image_paths:
+        if current_path != '':
+            page = site.joinpath(current_path)
+            page.parent.mkdir(parents=True, exist_ok=True)
+            page = replace_extension(page, ".html")
 
-
-@app.route("/<title>/<lang>/<chapter>")
-def get_page3(title, lang, chapter):
-    title = escape(title)
-    lang = escape(lang)
-    chapter = escape(chapter)
-    path = title + "/" + lang + "/" + chapter
-    return flask.render_template('pages.html', name='Chapters', path=path, items=os.listdir(pathlib.Path(library, title, lang, chapter)))
-
-
-@app.route("/<title>/<lang>/<chapter>/<page>")
-def get_page4(title, lang, chapter, page):
-    title = escape(title)
-    lang = escape(lang)
-    chapter = escape(chapter)
-    page = escape(page)
-    path = f"{library}/{title}/{lang}/{chapter}/{page}"
-    return flask.render_template('page.html', image_path=path)
-    # return send_file(path, mimetype='image/gif')
+            current_image = 'file:///' + pathlib.Path(current_path).absolute().as_posix()
+            replace_extension(page, ".html")
+            with open(page, 'w') as f:
+                out = html.format(prev=to_html_url(prev_path), current=current_image, next=to_html_url(next))
+                f.write(out)
+        prev_path = current_path
+        current_path = next_path
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
